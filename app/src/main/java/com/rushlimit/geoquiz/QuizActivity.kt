df67f802ace7f18d2e9f799d92caf6616adfdadd
Mohammed Amin <Mohammed.Amin@viacom.com>
2017-11-15 16:22:45 -0500

    Finish Chapter 2 Challenge 3

--- app/src/main/java/com/rushlimit/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/rushlimit/geoquiz/QuizActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,8 +12,8 @@ class QuizActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = arrayOf(
